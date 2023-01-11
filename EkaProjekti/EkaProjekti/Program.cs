25ac16e7e542d4812b08e24d22e3d1cb51e2bfcb
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2023-01-11 14:38:53 +0200

    eka projekti

---
+++ EkaProjekti/EkaProjekti/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EkaProjekti
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Maailma!");
+        }
+    }
+}
+
