03a01e9eb2d694bb83be710c83ca055358d7f018
Opiskelija <ville.saarinen@edu.keuda.fi>
2023-01-12 08:10:22 +0000

    projekti01

---
+++ Projekti01/SyotaNimi/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ole hyvä ja anna etunimesi: ");
+        string enimi = Console.ReadLine();
+        Console.Write("Ole hyvä ja anna sukunimesi: ");
+        string snimi = Console.ReadLine();
+        Console.WriteLine("Hei, " + enimi + " " + snimi);
+        Console.WriteLine("Hei, {0} {1}", enimi, snimi);
+    }
+}
