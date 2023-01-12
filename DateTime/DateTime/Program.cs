b08fe3c89f4c4075ba578abe9fe8b4ab4d321d84
Opiskelija <ville.saarinen@edu.keuda.fi>
2023-01-12 09:17:41 +0000

    suomiaika

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Globalization;
+
+DateTime date1 = DateTime.Now;
+Console.WriteLine(date1);
+
+string formatted = date1.ToString("dd/MM/yyyy hh:mm");
+Console.WriteLine(formatted);
+
+
+
+
+
+
