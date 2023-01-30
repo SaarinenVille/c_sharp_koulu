6d87e39daa71313683f476ca52544b1ea5c150ad
SaarinenVille <ville.saarinen@edu.keuda.fi>
2023-01-30 21:02:46 +0200

    CRUD tehtävä 5

---
+++ CRUD-projekti/LukujenJarjestys/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LukujenJarjestys
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.uusiLukuTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(67, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku (-999 lopetus):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // uusiLukuTB
+            // 
+            this.uusiLukuTB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.uusiLukuTB.Location = new System.Drawing.Point(341, 103);
+            this.uusiLukuTB.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.uusiLukuTB.Name = "uusiLukuTB";
+            this.uusiLukuTB.Size = new System.Drawing.Size(237, 35);
+            this.uusiLukuTB.TabIndex = 1;
+            this.uusiLukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uusiLukuTB_KeyPress);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(67, 163);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(83, 30);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 900);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.uusiLukuTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "LukujenJarjestys";
+            this.Text = "Lukujen järjestys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox uusiLukuTB;
+        private Label VastausLB;
+    }
+}
