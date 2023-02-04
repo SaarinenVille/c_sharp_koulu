7ddf30f18e61deabe5dcdc5bcdf921e2e95fda1d
SaarinenVille <ville.saarinen@edu.keuda.fi>
2023-02-04 14:46:22 +0200

    Nopat formin teko ja koodin aloitus

---
+++ CRUD-projekti/NopanHeitto/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NopanHeitto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Noppa01PB = new System.Windows.Forms.PictureBox();
+            this.Noppa02PB = new System.Windows.Forms.PictureBox();
+            this.heitaBT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa01PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa02PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(570, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nopan heitto";
+            // 
+            // Noppa01PB
+            // 
+            this.Noppa01PB.Image = global::NopanHeitto.Properties.Resources.dice01;
+            this.Noppa01PB.Location = new System.Drawing.Point(475, 166);
+            this.Noppa01PB.Name = "Noppa01PB";
+            this.Noppa01PB.Size = new System.Drawing.Size(220, 164);
+            this.Noppa01PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Noppa01PB.TabIndex = 1;
+            this.Noppa01PB.TabStop = false;
+            // 
+            // Noppa02PB
+            // 
+            this.Noppa02PB.Image = global::NopanHeitto.Properties.Resources.dice01;
+            this.Noppa02PB.Location = new System.Drawing.Point(701, 166);
+            this.Noppa02PB.Name = "Noppa02PB";
+            this.Noppa02PB.Size = new System.Drawing.Size(220, 164);
+            this.Noppa02PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Noppa02PB.TabIndex = 2;
+            this.Noppa02PB.TabStop = false;
+            // 
+            // heitaBT
+            // 
+            this.heitaBT.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.heitaBT.Location = new System.Drawing.Point(630, 371);
+            this.heitaBT.Name = "heitaBT";
+            this.heitaBT.Size = new System.Drawing.Size(136, 46);
+            this.heitaBT.TabIndex = 3;
+            this.heitaBT.Text = "Heitä";
+            this.heitaBT.UseVisualStyleBackColor = false;
+            this.heitaBT.Click += new System.EventHandler(this.heitaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1512, 847);
+            this.Controls.Add(this.heitaBT);
+            this.Controls.Add(this.Noppa02PB);
+            this.Controls.Add(this.Noppa01PB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Nopan heitto";
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa01PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa02PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox Noppa01PB;
+        private PictureBox Noppa02PB;
+        private Button heitaBT;
+    }
+}
---
+++ CRUD-projekti/NopanHeitto/Form1.cs
@@ -0,0 +1,21 @@
+namespace NopanHeitto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void heitaBT_Click(object sender, EventArgs e)
+        {
+            piirraNoppa(Noppa01PB);
+            piirraNoppa(Noppa02PB);
+        }
+
+        private void piirraNoppa(PictureBox NoppaBox)
+        {
+
+        }
+    }
+}
